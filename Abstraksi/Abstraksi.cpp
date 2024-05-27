9625f8a05c3295aaee9d46ae99b7e2f3688f9a92
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-05-27 14:34:47 +0700

    menambahkan void setXY

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 class AbstraksiClass {
 private: string x, y;
+
+public:
+	//method untuk mengisi nilai
+	//private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
