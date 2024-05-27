fbcc747b3d6aaa1592c723ecfdf0a7a39012f4fb
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-05-27 15:06:25 +0700

    membuat class pelajar

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -12,12 +12,22 @@ public:
 	}
 };
 
-class pekerja : virtual public orang {
+class pekerja :  public orang {
 public:
 
 	pekerja(int pUmur) :
 		orang(pUmur)
 	{
 		cout << "Pekerja dibuat\n" << endl;
+	}
+};
+
+class pelajar : virtual public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "Pelajar dibuat\n" << endl;
 	}
 };
